190e794a8e0de8670571ed796f2f4a039f070fa0
Ubuntu <ubuntu@ip-172-31-18-86.ca-central-1.compute.internal>
2020-04-27 23:14:58 +0000

    create model directory

--- maadmin.js
+++ maadmin.js
@@ -3,15 +3,14 @@
 var port = 3000;
 
 var express = require('express');
-var cookieParser = require('cookie-parser');
 var path = require('path');
 var bodyParser = require('body-parser');
+var models = require('models');
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, function () {
-    console.log('Example app listening on port '+port);
+    console.log('maadmin app listening on port '+port);
   });
---
+++ model/league.js
@@ -0,0 +1,8 @@
+
+
+class Leagues{
+    constructor(){
+        this.teams = []
+        this.divisions = {}
+    }
+}
