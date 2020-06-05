c043dd0fc51465b918c0cea40d3dde7bfa99fc4f
Osama <osama.hafez@mail.utoronto.ca>
2020-06-04 20:26:12 -0400

    build for new look

---
+++ client/build/precache-manifest.2b1089bce7af22fd48899b0fa49e4d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe16d3d4ab336d352d2c6e80f5e4d20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9548e8e8c0c89ef38a8",
+    "url": "/static/css/main.6ba1bdc5.chunk.css"
+  },
+  {
+    "revision": "391215d0898b83ef7c71",
+    "url": "/static/js/2.73d6f4d0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.73d6f4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9548e8e8c0c89ef38a8",
+    "url": "/static/js/main.5e20a107.chunk.js"
+  },
+  {
+    "revision": "ca25aded7950b9337894",
+    "url": "/static/js/runtime-main.95463b37.js"
+  }
+]);
