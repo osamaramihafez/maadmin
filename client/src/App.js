ee44b677ebf310f39eb17fd60b5e8be20d2bf251
Osama <osama.hafez@mail.utoronto.ca>
2020-06-22 19:17:13 -0400

    restyled client for easier eyes, added logo, fixed minor bugs

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LeagueList from './Components/LeagueList.js'
 import './App.css'
 import Button from 'react-bootstrap/Button';
 import axios from 'axios'
+import logo from './LogoRightSide.png'
 
 export class App extends Component {
     
@@ -52,18 +53,18 @@ export class App extends Component {
             {!this.state.loggedIn
                     ? (!this.state.create
                         ? <div className='Title'>
-                            <h1>Maadmin</h1>
+                            <img src={logo} alt=''></img>
                             <Button className='logout' variant='dark' onClick={this.create.bind(this)}>Create an Account</Button>
                             <h6>A league managment tool. Still under development.</h6>
                         </div>
                         : <div className='Title'>
-                            <h1>Maadmin</h1>
+                            <img src={logo} alt=''></img>
                             <Button className='logout' variant='dark' onClick={this.created.bind(this)}>Back to login</Button>
                             <h6>A league managment tool. Still under development.</h6>
                         </div>
                     )
                     :<div className='Title'>
-                        <h1>Maadmin</h1>
+                        <img src={logo} alt=''></img>
                         <Button className='logout' variant='dark' onClick={this.logout.bind(this)}>Logout</Button>
                         <h6>A league managment tool. Still under development.</h6>
                     </div>
